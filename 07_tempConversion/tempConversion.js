b773fb0e5ce470ea0d5e11b09d752c6e7e110414
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-12-17 19:00:18 +0000

    TDD in javascript

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farh) {
+
+  if (farh === 32) return 0;
+  let celsius = ((farh - 32) * 5/9).toFixed(1);
+  return +celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+
+  if (celc === 0) return 32;
+  let fahrenheit = ((celc *(9/5)) + 32).toFixed(1);
+
+  return +fahrenheit;
 };
 
 // Do not edit below this line
